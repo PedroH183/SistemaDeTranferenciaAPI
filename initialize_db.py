a336a89a78c50fa549078ace60b7ba190c850f06
PedroH183 <pedro_freires18@outlook.com>
2024-02-15 20:58:50 -0300

    feat: improve code of initialize_db.py

--- initialize_db.py
+++ initialize_db.py
@@ -1,20 +1,20 @@
-from run import engine, Base, session
-from app.Model import UsuariosComunModel, UsuariosLojistaModel
-
+from run import engine, Base
+from app.Domain.UsuariosComumModel import UsuarioComum
+from app.Domain.UsuariosLojistaModel import Lojista
 
 def init_db():
-  """
-    Função responsável por criar todas as tabelas no banco de dados.
-  """
-  
-  # delete all tables 
-  Base.metadata.drop_all(bind=engine)
-  
-  # create all tables
-  Base.metadata.create_all(bind=engine)
-  
-  print("Initialized the db")
+    """
+      Função responsável por criar todas as tabelas no banco de dados.
+    """
+
+    # delete all tables
+    Base.metadata.drop_all(bind=engine)
+
+    # create all tables
+    Base.metadata.create_all(bind=engine)
+
+    print("Initialized the db")
 
 
 if __name__ == "__main__":
-  init_db()
+    init_db()
