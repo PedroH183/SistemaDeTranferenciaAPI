c3e71ff339613d2390801eee4b2faaeac4bb6da0
PedroH183 <pedro_freires18@outlook.com>
2024-02-15 20:57:57 -0300

    chore: improve entry code execution.

--- run.py
+++ run.py
@@ -3,21 +3,22 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1234@localhost:5432/archlimpa'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
 session = scoped_session(
-  sessionmaker( bind=engine, autocommit=False, autoflush=False )
+    sessionmaker(bind=engine, autocommit=False, autoflush=False)
 )
 
 Base = declarative_base()
 
-from app.Controller.usuarioController import *
-from app.Controller.lojistaController import *
-from app.Model.UsuariosComunModel import UsuarioComun
-from app.Model.UsuariosLojistaModel import Lojista
+## Imports Controller Area
+from app.Controller.UsuarioController import *
+from app.Controller.LojistaController import *
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port="5000", debug=True) # type: ignore
+    app.run(host="0.0.0.0", port="5000", debug=True)  # type: ignore
